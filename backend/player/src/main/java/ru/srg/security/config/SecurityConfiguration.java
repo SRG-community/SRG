f14c2930c8e854d3a4f39aed5f9d0110f693997a
alexfranz <ut1339@gmail.com>
2017-10-04 01:41:47 +0300

    VUEX #1: - back changes was rollback

--- backend/player/src/main/java/ru/srg/security/config/SecurityConfiguration.java
+++ backend/player/src/main/java/ru/srg/security/config/SecurityConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.savedrequest.NullRequestCache;
-import org.springframework.social.security.SpringSocialConfigurer;
 
 @Configuration
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
@@ -27,16 +26,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
             .and()
-            .csrf()
-            .and()
-            .apply(getSpringSocialConfigurer());
+            .csrf().disable();
     }
-
-    private SpringSocialConfigurer getSpringSocialConfigurer() {
-        SpringSocialConfigurer config = new SpringSocialConfigurer();
-        config.alwaysUsePostLoginUrl(true);
-        config.postLoginUrl("/mygames");
-        return config;
-    }
-
 }
